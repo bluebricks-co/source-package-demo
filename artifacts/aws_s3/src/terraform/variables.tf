c0203745093f2128ba64faca38b2bdc1597f63b8
Yevhenii Tiutiunnyk <yevhenii@pillar.security>
2026-01-16 15:55:46 +0200

    fix artifact

--- artifacts/aws_s3/src/terraform/variables.tf
+++ artifacts/aws_s3/src/terraform/variables.tf
@@ -1,10 +1,8 @@
-
-variable "acl" {
-  default     = "private"
-  description = "The canned ACL to apply"
-  type        = string
+variable "attach_policy" {
+  default     = false
+  description = "Controls if S3 bucket should have bucket policy attached (set to `true` to use value of `policy` as bucket policy)"
+  type        = bool
 }
-
 
 variable "block_public_acls" {
   default     = true
@@ -12,40 +10,11 @@ variable "block_public_acls" {
   type        = bool
 }
 
-
 variable "block_public_policy" {
   default     = true
   description = "Whether Amazon S3 should block public bucket policies for this bucket"
   type        = bool
 }
-
-
-variable "name" {
-  description = "The S3 bucket name"
-  type        = string
-}
-
-
-variable "cors_rule" {
-  default     = []
-  description = "Specifies the CORS configuration for the bucket"
-  type        = any
-}
-
-
-variable "create_bucket" {
-  default     = true
-  description = "Controls if S3 bucket should be created"
-  type        = bool
-}
-
-
-variable "force_destroy" {
-  default     = false
-  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
-  type        = bool
-}
-
 
 variable "ignore_public_acls" {
   default     = true
@@ -53,6 +22,28 @@ variable "ignore_public_acls" {
   type        = bool
 }
 
+variable "restrict_public_buckets" {
+  default     = true
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket"
+  type        = bool
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket"
+  type        = string
+}
+
+variable "cors_rule" {
+  default     = []
+  description = "Specifies the CORS configuration for the bucket"
+  type        = any
+}
+
+variable "create_bucket" {
+  default     = true
+  description = "Controls if S3 bucket should be created"
+  type        = bool
+}
 
 variable "lifecycle_rule" {
   default     = []
@@ -60,37 +51,40 @@ variable "lifecycle_rule" {
   type        = any
 }
 
-
 variable "logging" {
   default     = {}
   description = "Map containing access bucket logging configuration."
   type        = map(string)
 }
 
+variable "policy" {
+  default     = ""
+  description = "A valid bucket policy JSON document"
+  type        = string
+}
 
 variable "region" {
   type = string
+  default = "eu-west-1"
 }
 
+variable "versioning" {
+  default = {
+    enabled = true
+  }
 
-variable "restrict_public_buckets" {
-  default     = true
-  description = "Whether Amazon S3 should restrict public bucket policies for this bucket"
-  type        = bool
-}
-
-
-variable "server_side_encryption_configuration" {
-  default     = {}
-  description = "Map containing server-side encryption configuration."
+  description = "Versioning configuration block"
   type        = any
 }
 
-
-variable "versioning" {
-  default = true
-  description = "Versioning configuration block"
-  type        = bool
+variable "website" {
+  default     = {}
+  description = "Map containing static web-site hosting or redirect configuration"
+  type        = any
 }
 
-
+variable "tags" {
+  default =  {}
+  description = "A mapping of tags to assign to the bucket."
+  type = map(string)
+}
