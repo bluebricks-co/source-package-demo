bd5b01cdec64a57a0cd0b3f79f42e2aa4c7e2940
Hai <hai@Hais-MacBook-Pro.local>
2026-03-20 12:35:38 -0700

    feat: enable versioning for S3 bucket test0for0the0demo0now

--- artifacts/aws_s3/src/terraform/s3.tf
+++ artifacts/aws_s3/src/terraform/s3.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "demo" {
   bucket = var.name
   tags = var.tags
 }
+
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
